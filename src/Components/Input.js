d00748b325cc00ab2a0877a09cf6af1ac0979958
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-02-23 14:13:24 +0100

    Added events

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,9 +1,19 @@
+/* eslint-disable react/prop-types */
 import './Input.css';
 
-const Input = () => (
-  <div className="input">
-    <h1>0</h1>
-  </div>
-);
+const Input = ({ text }) => {
+  const { total, next, operation } = text;
+  return (
+    <div className="input">
+      <h1>
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+      </h1>
+    </div>
+  );
+};
 
 export default Input;
--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,12 +1,19 @@
 import './calculator.css';
+import { useState } from 'react';
 import Button from './button';
 import Input from './Input';
+import calculate from './logic/calculate';
 
 function Calculator() {
+  const [text, setText] = useState({ total: 0, next: '', operation: '' });
+  const onClickHandler = (event) => {
+    const newText = calculate(text, event.target.value);
+    setText(newText);
+  };
   return (
     <div className="calc-container">
-      <Input />
-      <Button />
+      <Input text={text} />
+      <Button Action={onClickHandler} />
     </div>
   );
 }
